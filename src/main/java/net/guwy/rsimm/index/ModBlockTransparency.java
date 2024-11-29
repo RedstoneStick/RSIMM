299b3f8b553d84c07f686a9f095041aeaf835348
RedstoneStick <37273264+RedstoneStick@users.noreply.github.com>
2024-11-29 22:54:23 +0200

    + 1.21.1 update

--- src/main/java/net/guwy/rsimm/index/ModBlockTransparency.java
+++
@@ -1,7 +0,0 @@
-package net.guwy.rsimm.index;
-
-public class ModBlockTransparency {
-    public static void init() {
-        //ItemBlockRenderTypes.setRenderLayer(ModBlocks.PALE_CREAM_DOOR.get(), RenderType.cutout());
-    }
-}
